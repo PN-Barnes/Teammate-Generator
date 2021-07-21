67c0ce5ce58c5e016be6c0c23fa6d199da0636d1
PN-Barnes <paulieb99@gmail.com>
2021-07-20 18:06:29 -0700

    created the manager class and engineer class to take parameters nd make the employee

--- index.js
+++ index.js
@@ -1,53 +1,30 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const Employee = require('./lib/Employee')
 
-const questions = [{
-    type: 'input',
-    message: 'What is the title of the Readme?',
-    name: "title",
-},
-{
-    type: 'input',
-    message: 'Please Write the description of your project.',
-    name: 'description'
-},
-{
-    type:'input',
-    message: 'Please write the install instructions needed for this application. preferable step-by-step details.',
-    name: 'install'
+const questions = [
+    {
+        type: 'list',
+        message: 'Please select your role.',
+        choices: ['Manager', 'Engineer', 'Intern'],
+        name: 'role'
+    },
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: "name",
+    },
+    {
+        type: 'input',
+        message: 'What is your ID',
+        name: 'id'
+    },
+    {
+        type:'input',
+        message: 'What is your email',
+        name: 'email'
 
-},
-{
-    type: 'input',
-    message: 'Please add the usage examples for the application, format for including screenshots: ![title](filePath)',
-    name: 'usage'
-},
-{
-    type: 'list',
-    message: 'Please select a license used for this project application',
-    choices:['MIT', 'Unlicense', 'Mozilla', 'Apache', 'BSD-2','BSD-3', 'Boost', 'none'],
-    name: 'license'
-},
-{
-    type: 'input',
-    message: 'Please specify the guidelines for implementing contributions to this project', 
-    name: 'contributing'
-},
-{
-    type: 'input',
-    message: 'Please write any tests for this application',
-    name: 'tests'
-},
-{
-    type: 'input',
-    message: 'what is your github Username?',
-    name: 'gitHubUN'
-},
-{
-    type: 'input',
-    message: 'What is your email?',
-    name: 'email'
-}
+    },
 ];
 
 inquirer
@@ -55,3 +32,11 @@ inquirer
     .then((Response)=> {
         console.log(Response)
     })
+
+
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) =>
+          err ? console.error(err) : console.log('Success!')
+        );
+    
+    }
