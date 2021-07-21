0fd34e36f6657246705763b0f82ef0dfb64e571f
PN-Barnes <paulieb99@gmail.com>
2021-07-20 19:45:24 -0700

    added array of questions for role and started to create runGenerator funciton to start process

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const Employee = require('./lib/Employee')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Engingeer = require('./lib/Engineer')
+const { type } = require('os')
+
+const teamMembers = [];
 
 const questions = [
-    {
-        type: 'list',
-        message: 'Please select your role.',
-        choices: ['Manager', 'Engineer', 'Intern'],
-        name: 'role'
-    },
     {
         type: 'input',
         message: 'What is your name?',
@@ -23,20 +24,96 @@ const questions = [
         type:'input',
         message: 'What is your email',
         name: 'email'
-
+        
     },
+    {
+        type: 'list',
+        message: 'Please select your role.',
+        choices: ['Manager', 'Engineer', 'Intern'],
+        name: 'role'
+    }
 ];
 
-inquirer
-    .prompt(questions)
-    .then((Response)=> {
-        console.log(Response)
-    })
+const officeNumber = [
+    {
+        type: 'input',
+        message: 'What is your office number?',
+        name: 'officeNumber'
+    }
+]
 
+const schoolName = [
+    {
+        type: 'input',
+        message: 'What school did you attend?',
+        name: 'school'
+    }
+]
 
+const gitHub = [
+    {
+        type: 'input',
+        message: 'What is your Github Username?',
+        name: 'github'
+    }
+]
     function writeToFile(fileName, data) {
         fs.writeFile(fileName, data, (err) =>
           err ? console.error(err) : console.log('Success!')
         );
     
     }
+const addEmployee = [
+    {
+        type: 'confirm',
+        message: 'would you like to add more team members?',
+        name:'addEmployee',
+        default: false
+    }
+]
+async function RunGenerator() {
+    let mainQuestions = await inquirer.prompt(questions)
+
+    let roleChoice = await decideRole(mainQuestions);
+
+    let roleAnswers = await inquirer.prompt(roleChoice)
+
+    let EmployeeInfo = await {mainQuestions, roleAnswers}
+    
+    let createEmployee = await generateEmployee(EmployeeInfo)
+
+    teamMembers.push(createEmployee)
+
+    let addMember = await inquirer.prompt(addEmployee);
+
+    continue(addEmployee.confirm)
+}
+
+function generateEmployee(info) {
+    let name = info.name;
+    let id = info.id;
+    let email = info.email;
+    let role = info.role;
+
+    switch(role) {
+        case 'Manager':
+            return new Manager(name, id, email, info.officeNumber);
+        case 'Engineer':
+            return new Engingeer(name, id, email, info.gitHub);
+        case 'Intern':
+            return new Intern(name, id, email, info.school)
+    }
+}
+
+function decideRole(Employee) {
+    let role = Employee.role
+    switch(role) {
+        case 'Manager':
+            return officeNumber;
+        case 'Engineer':
+            return gitHub;
+        case 'Intern':
+            return schoolName;
+    }
+}
+runGenerator();
